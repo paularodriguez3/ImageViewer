2123dc014e924d731d8fe3c5d9635ac7b928f0b7
paularodriguez3 <paularosarodriguezmorales03@gmail.com>
2024-01-03 19:37:59 +0000

    Third commit.

--- src/main/java/software/ulpgc/imageviewer/control/Main.java
+++ src/main/java/software/ulpgc/imageviewer/control/Main.java
@@ -7,7 +7,7 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        File folder = new File("C:/Users/paula/Downloads/imagenes");
+        File folder = new File("C:/Users/paula/IdeaProjects/ImageViewerPaula/src/main/resources");
         FileImageLoader imageLoader = new FileImageLoader(folder);
         Image image = imageLoader.load();
         MainFrame mainFrame = new MainFrame(imageLoader);
